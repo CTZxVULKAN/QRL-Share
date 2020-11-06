21540c2fb1d44de37b18f01f90dbe7b3edfe6880
BiswasJishnu <biswasjishnu24@gmail.com>
2020-11-06 20:43:12 +0530

    created basic extension

---
+++ qr_generator.js
@@ -0,0 +1,16 @@
+if (browser.tabs && browser.tabs.query) {
+	function getActiveTab() {
+		return browser.tabs.query({currentWindow: true, active: true});
+	}
+	getActiveTab().then(function (data) {
+		data = data[0]; /* only tab in set */
+		var el = kjua({
+			render: 'convas',
+			text: data.url,
+			size:250,
+		});
+		el.className = "qrcode"
+		el.style = "width:100%; height: 100%";
+		document.querySelector('body').appendChild(el);
+	});
+}
